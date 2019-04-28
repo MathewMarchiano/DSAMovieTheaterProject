21af72a5bd190a2dfe9ccc835a1554c7d379930e
Mathew R. Marchiano <44630082+MathewMarchiano@users.noreply.github.com>
2019-04-27 23:47:26 -0400

    Delete StackException.java

--- StackException.java
+++
@@ -1,8 +0,0 @@
-package structs;
-
-public class StackException
-    extends java.lang.RuntimeException {
-    public StackException(String s) {
-        super(s);
-    }  // end constructor
-}  // end StackException
